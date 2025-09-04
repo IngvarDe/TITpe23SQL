80c14780f322ca8fa8e233c48b8a9c206dadd90b
MSI\ingva <inde999@gmail.com>
2025-09-04 18:34:13 +0300

    04.09.2025

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,3 +1,6 @@
+--1 tund
+-- 03.09.2025
+
 --loome andmebaasi
 create database TITpe23
 
@@ -136,7 +139,141 @@ select * from Person order by Name desc
 select top 3 * from Person
 
 -- rida 131
--- 03.09.2025
+-- 04.09.2025
 -- tund 2
 
+-- kolm esimest, aga tabeli järjestus on Age ja siis Name
+select * from Person
+select top 3 Age, Name from Person
+
+-- n'ita esimesed 50% tabelis
+select top 50 percent * from Person
+
+--järjestab vanuse järgi isikud
+select * from Person order by Age desc
+
+--võtab neli esimest ja järjestab vanuse järgi
+select top 4 * from Person order by Age
+
+-- muudab Age muutuja intiks ja näitab vanuselises järjestuses
+select * from Person order by cast(Age as int)
+
+-- kõikide isikute koondvanus
+select sum(cast(Age as int)) from Person
+
+--kuvab kõige nooremat isikut
+select min(cast(Age as int)) from Person
+--kuvab kõige vanemat isikut
+select max(cast(Age as int)) from Person
+
+-- näeme konkreetsetes linnades olevate isikute koondvanust
+-- enne oli Age string, aga enne päringut muutsime selle int-ks
+select City, sum(Age) as TotalAge from Person group by City 
+
+--kuidas saab koodiga muuta andmetüüpi ja selle pikkust
+--vaja muuta Name nvarchar(25) peale
+alter table Person
+alter column Name nvarchar(25)
+
+--- kuvab esimeses reas välja toodud järjestuses ja kuvab Age-i TotalAge-ks
+--- järjest City-s olevate nimede järgi ja siis genderId järgi
+select City, GenderId, sum(Age) as TotalAge from Person
+group by City, GenderId order by City
+
+--näitab, et mitu rida on selles tabelis
+select count(*) from Person
+select * from Person
+
+-- tahame tulemust, et mitu inimest on genderId väärtusega 2
+-- konkreetses linnas ja arvutab vanuse kokku
+select GenderId, City, sum(Age) as TotalAge, count(Id) as [Total person(s)]
+from Person
+where GenderId = '1'
+group by GenderId, City
+
+--näitab ära inimeste koondvanuse, mis on üle 41 a ja 
+--kui palju neid igas linnas elab 
+--eristab inimese soo järgi ära
+select GenderId, City, sum(Age) as TotalAge, count(Id)
+as [Total Person(s)]
+from Person
+group by GenderId, City having sum(Age) > 41
+
+-- loome tabelid Employees ja Department
+--Department tabelis peavad olema muutujad:
+--Id int primary key,
+--DepartmentName nvarchar(50),
+--Location nvarchar(50),
+--DepartmentHead nvarchar(50)
+
+--Employees tabelis peavad olema muutujad:
+--Id int primary key,
+--Name nvarchar(50),
+--Gender nvarchar(50),
+--Salary nvarchar(50),
+--DepartmentId int
+
+create table Department
+(
+Id int primary key,
+DepartmentName nvarchar(50),
+Location nvarchar(50),
+DepartmentHead nvarchar(50)
+)
+
+create table Employees
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(50),
+Salary nvarchar(50),
+DepartmentId int
+)
+
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (1, 'Tom', 'Male', 4000, 1),
+(2, 'Pam', 'Female', 3000, 3),
+(3, 'John', 'Male', 3500, 1),
+(4, 'Sam', 'Male', 4500, 2),
+(5, 'Todd', 'Male', 2800, 2),
+(6, 'Ben', 'Male', 7000, 1),
+(7, 'Sara', 'Female', 4800, 3),
+(8, 'Valarie', 'Female', 5500, 1),
+(9, 'James', 'Male', 6500, NULL),
+(10, 'Russell', 'Male', 8800, NULL)
+
+insert into Department(Id, DepartmentName, Location, DepartmentHead)
+values 
+(1, 'IT', 'London', 'Rick'),
+(2, 'Payroll', 'Delhi', 'Ron'),
+(3, 'HR', 'New York', 'Christie'),
+(4, 'Other Department', 'Sydney', 'Cindrella')
+
+select * from Department
+select * from Employees
+
+--- join päring
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+
+select sum(cast(Salary as int)) from Employees  --arvutab k]ikide palgad kokku
+
+-- min palga saaja ja kui panen min asemele max, siis max palga saaja
+select min(cast(Salary as int)) from Employees
+
+--[he kuu lõikes palgafond linnade lõikes
+select Location, sum(cast(Salary as int)) as TotalSalary
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+group by Location 
+
+-- lisame Employees tabelisse City veeru ja peab olema nvarchar(30)
+alter table Employees
+add City nvarchar(30)
+
+-- rida 257
+--tund 3
 
